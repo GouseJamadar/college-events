36e8079049bb0f19dd8ee90520b2acf06ad71a31
GouseJamadar <gouse.jamadar07@gmail.com>
2025-12-10 17:54:17 +0530

    Simplify CORS

--- backend/server.js
+++ backend/server.js
@@ -9,22 +9,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: function (origin, callback) {
-    const allowedOrigins = [
-      process.env.FRONTEND_URL,
-      'http://localhost:3000',
-      'https://college-events-kn3q-git-main-mohammed-gouses-projects.vercel.app',
-      'https://college-events-kn3q.vercel.app'
-    ];
-    if (!origin || allowedOrigins.some(allowed => origin.startsWith(allowed?.replace(/\/$/, '')))) {
-      callback(null, true);
-    } else {
-      callback(null, true); // Allow all for now
-    }
-  },
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
